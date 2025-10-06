5681b68c20bbbde8c9b452af03f3e91905bc4b8a
Mems2001 <mems2001code@gmail.com>
2025-10-06 12:06:25 -0500

    feature: new sudoku producing algorithm why: I am pursuing better algorithms to produce sudokus, ideally, one that can produce a good sudoku purely by following its rules and with one try. Right now i am perfecting the backtracking algorithm. what: A backtracking algorithm that keeps track of all posible numbers at each location to avoid a safe checking function and looking towards better perfomance if posible. impact: Ideally, a more efficient algorithm.

--- utils/createSudoku3.js
+++ utils/createSudoku3.js
@@ -1,5 +1,4 @@
 class Sudoku {
-    previous_posibilities_grids = []
 
     constructor () {
         this.grid = this.generateEmptyGrid()
@@ -35,7 +34,7 @@ class Sudoku {
     }
 
     /** Instead of checking for safety at each random attempted number we keep the posible numbers at each location. So, when we place any number at the sudoku grid we also remove that number from the arrays of posible numbers correspondig the same row, column and quadrant. That way any time we were to attempt a random number that random number would be guaranteed to be safe. */
-    removeFromPosibilities(number, row, col) {
+    removeFromPosibilities(grid, number, row, col) {
         // Same 3x3 quadrant posibilities
         const startRow = row - row % 3
         const startCol = col - col % 3
@@ -43,21 +42,23 @@ class Sudoku {
             for (let j = 0; j < 3; j++) {
                 const r = startRow + i
                 const c = startCol + j
-                const index = this.posibilities_grid[r][c].indexOf(number)
-                if (index >= 0) this.posibilities_grid[r][c].splice(index, 1)
+                const index = grid[r][c].indexOf(number)
+                if (index >= 0) grid[r][c].splice(index, 1)
             }
         }
 
         // Same row posibilities
         for (let n = 0; n < 9; n++) {
-            const index = this.posibilities_grid[row][n].indexOf(number)
-            if (index >= 0) this.posibilities_grid[row][n].splice(index, 1)
+            const index = grid[row][n].indexOf(number)
+            if (index >= 0) grid[row][n].splice(index, 1)
         }
         // Same column posibilities
         for (let n = 0; n < 9; n++) {
-            const index = this.posibilities_grid[n][col].indexOf(number)
-            if (index >= 0) this.posibilities_grid[n][col].splice(index, 1)
+            const index = grid[n][col].indexOf(number)
+            if (index >= 0) grid[n][col].splice(index, 1)
         }
+
+        return grid
     }
 
     /**
@@ -66,22 +67,19 @@ class Sudoku {
      * @param {*} row The index of the grid array elements, wich represents a row position.
      * @param {*} col The index of the row array elements, wich represents a column position.
      */
-    asignNumber(number, row, col) {
+    asignNumber(posibilities_grid, number, row, col) {
         // console.log('---> attempted number:', number, row, col)
-        if (this.posibilities_grid[row][col].includes(number)) {
-            this.previous_posibilities_grids.push(JSON.parse(JSON.stringify(this.posibilities_grid)))
+        if (posibilities_grid[row][col].includes(number)) {
             this.grid[row].splice(col, 1, number)
-            this.removeFromPosibilities(number, row, col)
             return true
         } else {
-            // console.log('do not fit', this.posibilities_grid[row][col])
+            // console.log('do not fit', posibilities_grid[row][col])
             return false
         }
     }
 
     removeNumber(row, col) {
         this.grid[row].splice(col, 1, 0)
-        this.posibilities_grid = this.previous_posibilities_grids.pop()
     }
 
     /**
@@ -89,7 +87,7 @@ class Sudoku {
      * @param {*} grid A sudoku grid.
      * @returns A boolean, true if the grid is solved and false otherwise. This values enhances and allows the recursive behaviour.
      */
-    solveSudoku(grid) {
+    solveSudoku(grid, posibilities) {
         let row
         let col
         let isFilled = true
@@ -113,14 +111,15 @@ class Sudoku {
 
         // console.log('---> At:', row, col)
         // We shuffle the array of posible numbers to guarantee randomness at each cell.
-        const shuffled_array = this.shuffleArray([...this.posibilities_grid[row][col]])
+        const shuffled_array = this.shuffleArray([...posibilities[row][col]])
         console.log(shuffled_array)
         if (shuffled_array.length <= 0) return false
 
         // We try each posible number. For each, we then call the solveSudoku function again to tell if the sudoku is solvable given the number we are tying. Is solvable, we keep the number, if not we try the next number. When we are at a dead end the array of posible numbers will be empty, this is, the sudoku is not solvable, then we back-track. We back-track as well when any number of the array of posible numbers at the location leads us to a dead end. This means, we return to the previous location iteration, dicard the already tried number and keep trying with the other numbers. For example, suppose we are trying with number 5 at the [5][7] location. We will then fill the grid with 5 at [5][7] and then proceed to try the posible numbers at [5][8] (call again the solveSudoku function with the partially filled grid with 5 at [5][7]). If at the later location we found an empty array of posible numbers the algorithm returns to [5][7], discards 5 and try another number. If any of the numbers of the array of posible numbers at [5][7] leads us to a solution, then we return to [5][6] and try another number and so on, and iterate like that till we find a solvable path.
         for (const number of shuffled_array) {
-            this.asignNumber(number, row, col)
-            if (this.solveSudoku(grid)) {
+            this.asignNumber(posibilities, number, row, col)
+            const new_posibilities = this.removeFromPosibilities(JSON.parse(JSON.stringify(posibilities)), number, row, col)
+            if (this.solveSudoku(grid, new_posibilities)) {
                 return true
             }
             // console.log('---> number do not fit:', number, row, col)
@@ -131,7 +130,7 @@ class Sudoku {
     }
 
     generateSudoku() {
-        this.solveSudoku(this.grid)
+        this.solveSudoku(this.grid, JSON.parse(JSON.stringify(this.posibilities_grid)))
         return this.grid
     }
 }
